a8be9936f53ec5b0efd6dfb5049a0bb098b69ac9
ShivBhanKushWaha <shivbhankushwaha862003672001@gmail.com>
2024-01-31 23:55:09 +0530

    Shorts

--- screens/Shorts.jsx
+++ screens/Shorts.jsx
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,ScrollView ,Image} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Shorts = () => {
   return (
-    <View>
-      <Text>Shorts</Text>
-    </View>
+    <SafeAreaView className="flex-1 py-5 absolute">
+      <ScrollView showsVerticalScrollIndicator={false} >
+      <View >
+          <Image
+            source={require("../assets/avatar.jpeg")}
+            className="w-full h-screen"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
